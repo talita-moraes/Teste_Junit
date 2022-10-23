f3792c6ceb731a51a7a3271c4f540a8c448f432a
Talita Moraes Ferreira <43316963+talita-moraes@users.noreply.github.com>
2022-10-22 21:52:08 -0300

    Add files via upload
    primeiro commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import ogr.junit.Test
+public class Test {
+
+    @Test
+    public void testSomar() {
+        int a = 1;
+        int b = 2;
+        assertTrue( a + b == 3);
+    }
+}
